d7a085744fa383484818c33fbeaccc4965259bb3
daniel petcov <danielpetcov.work@gmail.com>
2025-09-10 17:28:57 +0300

    done structuring the basics of settings page

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,3 +1,39 @@
-export default function SettingsPage() {
-  return <div>hello settings</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+import SettingsForm from "./components/settings-form";
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+export default async function SettingsPage({ params }: SettingsPageProps) {
+  const param = await params;
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: param.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
 }
