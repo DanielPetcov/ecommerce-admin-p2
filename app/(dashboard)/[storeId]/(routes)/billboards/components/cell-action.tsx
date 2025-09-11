914e55bf1cf58cd29f281a340b910d6fd662bd4b
daniel petcov <danielpetcov.work@gmail.com>
2025-09-11 15:26:49 +0300

    done structuring the basics of the billboard tabel

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { BillboardColumn } from "./columns";
+
+interface CellActionProps {
+  data: BillboardColumn;
+}
+
+export default function CellAction({ data }: CellActionProps) {
+  return <div>asdfa</div>;
+}
